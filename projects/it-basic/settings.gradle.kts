6177164127e968a968a1e6a919f740f940417084
Adam <897017+aSemy@users.noreply.github.com>
2023-03-10 14:29:18 +0100

    simplify template settings.gradle.kts

--- projects/it-basic/settings.gradle.kts
+++ projects/it-basic/settings.gradle.kts
@@ -3,26 +3,8 @@
 rootProject.name = "it-basic"
 
 pluginManagement {
-  val dokka_it_kotlin_version: String by settings
-  val dokka_it_android_gradle_plugin_version: String? by settings
-
-//  plugins {
-//    id("org.jetbrains.kotlin.js") version dokka_it_kotlin_version
-//    id("org.jetbrains.kotlin.jvm") version dokka_it_kotlin_version
-//    id("org.jetbrains.kotlin.android") version dokka_it_kotlin_version
-//    id("org.jetbrains.kotlin.multiplatform") version dokka_it_kotlin_version
-//  }
-
-  resolutionStrategy {
-    eachPlugin {
-//      if (requested.id.id == "org.jetbrains.dokka") {
-//        useModule("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
-//      }
-
-    }
-  }
   repositories {
-    mavenLocal()
+    //mavenLocal()
     maven("https://cache-redirector.jetbrains.com/jcenter.bintray.com")
     mavenCentral()
     gradlePluginPortal()
