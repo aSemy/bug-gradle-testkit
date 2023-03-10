6988b82afc40414078ee6d67da86bee56314121d
Adam <897017+aSemy@users.noreply.github.com>
2023-03-10 14:37:59 +0100

    enable build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,10 +35,33 @@ dependencyResolutionManagement {
   }
 }
 
-//include(
-//  ":modules:integration-tests",
-//)
+plugins {
+  id("com.gradle.enterprise") version "3.12.3"
+}
 
+gradleEnterprise {
+
+  buildScan {
+    val isCI = providers.environmentVariable("CI").orNull.toBoolean()
+
+    tag(if (isCI) "CI" else "local")
+    tag(providers.systemProperty("os.name").orNull)
+    tag(providers.systemProperty("os.arch").orNull)
+
+    if (isCI) {
+      // only automatically enable build scan on CI
+      termsOfServiceUrl = "https://gradle.com/terms-of-service"
+      termsOfServiceAgree = "yes"
+      publishAlways()
+      isUploadInBackground = false
+
+      val ghServer = providers.environmentVariable("GITHUB_SERVER_URL").orNull
+      val ghRepo = providers.environmentVariable("GITHUB_REPOSITORY").orNull
+      val giRunId = providers.environmentVariable("GITHUB_RUN_ID").orNull
+      link("GitHub Workflow run", "$ghServer/$ghRepo/actions/runs/$giRunId")
+    }
+  }
+}
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
