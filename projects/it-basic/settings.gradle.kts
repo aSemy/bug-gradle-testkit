4351dddadbacc35ae152616204cacd8dd8dd3a8f
Adam <897017+aSemy@users.noreply.github.com>
2023-03-10 14:42:56 +0100

    add repos

--- projects/it-basic/settings.gradle.kts
+++ projects/it-basic/settings.gradle.kts
@@ -13,3 +13,29 @@ pluginManagement {
     maven("https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-dev")
   }
 }
+
+@Suppress("UnstableApiUsage") // Central declaration of repositories is an incubating feature
+dependencyResolutionManagement {
+
+  repositories {
+    mavenCentral()
+    google()
+
+    maven("https://www.jetbrains.com/intellij-repository/snapshots")
+    maven("https://www.jetbrains.com/intellij-repository/releases")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+    maven("https://www.myget.org/F/rd-snapshots/maven/")
+
+    ivy("https://github.com/") {
+      name = "GitHub Release"
+      patternLayout {
+        artifact("[organization]/[module]/archive/[revision].[ext]")
+        artifact("[organization]/[module]/archive/refs/tags/[revision].[ext]")
+        artifact("[organization]/[module]/archive/refs/tags/v[revision].[ext]")
+      }
+      metadataSources { artifact() }
+    }
+  }
+}
